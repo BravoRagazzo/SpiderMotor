1c6798f06c171383b72d1bfc88b4dba7cc5b06c1
matteoragni01 <matteo.ragni01@universitadipavia.it>
2023-05-26 10:35:47 +0200

    parameters

---
+++ constraints.m
@@ -0,0 +1,9 @@
+%% State constraint
+x1_max = 2;
+x1_min = -2;
+x2_max = 150;
+x2_min = -150;
+
+%% Control contraints
+u_max = 12;
+u_min = 0;
---
+++ motor.m
@@ -0,0 +1,18 @@
+clc
+clear
+close all
+
+%% Load parameters 
+parameters
+constraints
+
+%% Model
+
+A = [-Ra/L -Ke/L; Kt/J1 -fr/J1];
+B = [1/L 0]';
+C = eye(2);
+D = [0 0]';
+
+sys = ss(A,B,C,D);
+
+%%
