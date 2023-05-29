557ce4e1ce9a0a2d4b2a285ec4c68adca6e72eb8
matteoragni01 <matteo.ragni01@universitadipavia.it>
2023-05-29 14:06:40 +0200

    1 finished

--- motor.m
+++ motor.m
@@ -21,8 +21,8 @@ eig = eig(A);
 
 
 %% LQR continuous
-%Q_LQ = 100*[0.1 0; 0 10];
-Q_LQ = 100*eye(2);
+Q_LQ = 1000*[0.1 0; 0 10];
+%Q_LQ = 100*eye(2);
 R_LQ = 1;
 
 [K, S, CLP] = lqr(sys,Q_LQ,R_LQ);
@@ -35,13 +35,15 @@ B_tilda = [sysdis.B(1) Bw_bar(1,1) Bw_bar(1,2); sysdis.B(2) Bw_bar(2,1) Bw_bar(2
 D_tilda = [0 0 0 ; 0 0 0];
 
 
-Q_LQ = 100*[0.1 0;0 1];
+Q_LQ = 1000*[0 0;0 10];
 R_LQ = 1;
 
 [Kd, Sd, CLPd] = dlqr(sysdis.A,sysdis.B,Q_LQ,R_LQ);
 
+%%
+
+u = -(A(1,1)*(-A(2,2)*100/A(2,1)) + A(1,2)*100)*L
 
 
-
-
+i = -A(2,2)*100/A(2,1)
 
